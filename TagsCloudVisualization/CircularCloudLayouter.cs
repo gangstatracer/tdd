ffe44a5a1c3afb649990e3242df8ab7b46e1a7e3
iperevoschikov <iperevoschikov@skbkontur.ru>
2017-10-06 15:50:56 +0500

    TagsCloudVisualization project add

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        private readonly Point center;
+
+        CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        Rectangle PutNextRectange(Size rectangleSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
